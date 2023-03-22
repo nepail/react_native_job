5c31be5d5c7cf38746939f3ea7c758ca72643b64
Nepail <kls18z@hotmail.com>
2023-03-22 10:41:39 +0800

    add ScreenHeaderBtn

--- app/index.js
+++ app/index.js
@@ -22,11 +22,26 @@ const Home = () => {
             <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
           ),
           headerRight: () => (
-            <ScreenHeaderBtn iconUrl={icons.profile} dimension="100%" />
+            <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
           ),
           headerTitle: ""
         }}
       />
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View
+          style={{
+            flex: 1,
+            padding: SIZES.medium
+          }}>
+          <Welcome 
+          
+          />
+
+          <Popularjobs />
+          <Nearbyjobs />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
