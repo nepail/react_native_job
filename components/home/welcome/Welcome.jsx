f4fd67d78c2071acea3c4345082444ee14dec88f
Nepail <kls18z@hotmail.com>
2023-03-22 10:55:55 +0800

    add SearchBtn

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -25,8 +25,19 @@ const Welcome = () => {
         <View style={styles.searchWrapper}>
           <TextInput 
             style={styles.searchInput}
+            value=""
+            onChange={()=>{}}
+            placeholder="What are you looking for?"
           />
         </View>
+
+        <TouchableOpacity style={styles.searchBtn} onPress={()=>{}}>
+          <Image 
+            source={icons.search}
+            resizeMode="contain"
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
       </View>
     </View>
   );
