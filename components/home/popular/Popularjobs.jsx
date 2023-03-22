c860e9341e0913bfaf42a7c9ee1b8b4c08b3cc3f
Nepail <kls18z@hotmail.com>
2023-03-22 11:41:28 +0800

    add Fetch

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -12,10 +12,14 @@ import styles from "./popularjobs.style";
 import { COLORS, SIZES } from "../../../constants";
 import PopularJobCard from "../../common/cards/popular/PopularJobCard";
 
+import useFetch from "../../../hook/useFetch";
+
 const Popularjobs = () => {
   const router = useRouter();
-  const isLoading = false;
-  const error = false;
+  const { data, isLoading, error } = useFetch("search", {
+    query: "React developer",
+    num_pages: 1,
+  });
 
   return (
     <View style={styles.container}>
@@ -36,7 +40,7 @@ const Popularjobs = () => {
             data={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
             renderItem={(item) => <PopularJobCard item={item} />}
             keyExtractor={(item) => item?.job_id}
-            contentContainerStyle={{columnGap: SIZES.medium}}
+            contentContainerStyle={{ columnGap: SIZES.medium }}
             horizontal
           />
         )}
