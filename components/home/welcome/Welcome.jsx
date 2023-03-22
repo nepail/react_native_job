8a56cc2c923c22f194920b2ac5e548f64d179499
Nepail <kls18z@hotmail.com>
2023-03-22 10:45:09 +0800

    add Search

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -12,9 +12,22 @@ import styles from './welcome.style'
 import {icons, SIZES} from '../../../constants'
 
 const Welcome = () => {
+  const router = useRouter()
+
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Jim</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+      </View>
+
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput 
+            style={styles.searchInput}
+          />
+        </View>
+      </View>
     </View>
   );
 };
