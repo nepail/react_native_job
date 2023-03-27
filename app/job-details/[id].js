f999824e5859c8b28db3a98b683baa4732c7239d
Nepail <kls18z@hotmail.com>
2023-03-27 15:12:07 +0800

    add details-company

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -69,7 +69,13 @@ const JobDetails = () => {
                 <Text>No data</Text>
             ) : (
                 <View style={{padding: SIZES.medium, paddingBottom: 100}}>
-
+                    <Company 
+                        companyLogo={data[0].employer_logo}
+                        jobTitle={data[0].job_title}
+                        companyName={data[0].employer_name}
+                        location={data[0].job_country}
+                    />
+                    <JobTabs />
                 </View>
             )}
           </ScrollView>
