bf01916cab43585e7e73867d3bf111e0493b3f48
Nepail <kls18z@hotmail.com>
2023-03-27 14:26:31 +0800

    add Nearbyjobs

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -21,6 +21,12 @@ const Popularjobs = () => {
     num_pages: 1,
   });
 
+  const [selectedJob, setSelectedJob] = useState()
+
+  const handleCardPress = (item) => {
+
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -41,7 +47,8 @@ const Popularjobs = () => {
             renderItem={({item}) => 
               <PopularJobCard 
                 item={item} 
-  
+                selectedJob={selectedJob}
+                handleCardPress={handleCardPress}
               />
             }
             keyExtractor={(item) => item?.job_id}
